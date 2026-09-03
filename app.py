5874c4b1f6a3002549daec7eacbe29e5582f32d7
Cesar Yepes <90117882+CYFLZ@users.noreply.github.com>
2026-09-03 07:42:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,32 +12,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 mysql = MySQL(app)
 app.mysql = mysql
-
-# try:
-#     with mysql.connection.cursor() as cursor:
-#         cursor.execute("SELECT 1")
-#         result = cursor.fetchone()
-#         print("CONEXIÓN MYSQL OK:", result)
-# except Exception as e:
-#     print("ERROR MYSQL:", repr(e))
-
-try:
-    with app.app_context():
-        with mysql.connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
-            result = cursor.fetchone()
-            print("CONEXIÓN MYSQL OK:", result)
-except Exception as e:
-    print("ERROR MYSQL:", repr(e))
+ 
 
 
 load_routes(app) 
-
-print("MYSQL_HOST:", app.config.get("MYSQL_HOST"))
-print("MYSQL_PORT:", app.config.get("MYSQL_PORT"))
-print("MYSQL_USER:", app.config.get("MYSQL_USER"))
-print("MYSQL_DB:", app.config.get("MYSQL_DB"))
-print("MYSQL_SSL:", app.config.get("MYSQL_SSL"))
-print("CA existe:", os.path.exists("/etc/secrets/aiven-ca.pem"))
+ 
  
 # app.run(debug=True, port=5000, host='0.0.0.0')
