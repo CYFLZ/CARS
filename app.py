76ed3f059b08d9889ecd775809c83e90803eb82a
Cesar Yepes <90117882+CYFLZ@users.noreply.github.com>
2026-09-03 07:32:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from config import Config
 from Routes import load_routes
 from flask_mysqldb import MySQL
 
+print("MYSQL_HOST:", app.config.get("MYSQL_HOST"))
+print("MYSQL_PORT:", app.config.get("MYSQL_PORT"))
+print("MYSQL_USER:", app.config.get("MYSQL_USER"))
+print("MYSQL_DB:", app.config.get("MYSQL_DB"))
+print("MYSQL_SSL:", app.config.get("MYSQL_SSL"))
+print("CA existe:", os.path.exists("/etc/secrets/aiven-ca.pem"))
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 mysql = MySQL(app)
